3ac66e173b149e74af102ecc74a388545569984e
Oliver <olichase91@gmail.com>
2023-02-21 10:22:59 -0800

    Solved Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+
+let currentUser = 'Oli'
+let firstInitial = currentUser[0]
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+// const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() + `${currentUser}!`
+
+const shortGreeting = `Welcome, ${firstInitial}!`
+
+
+
